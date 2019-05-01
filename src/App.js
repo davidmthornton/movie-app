edb4572b69dbcf7be4d9d5cd3471a8540f40cc2b
davidmthornton <28890110+davidmthornton@users.noreply.github.com>
2019-05-01 15:33:08 +0100

    Improved how form data is used

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ class App extends Component {
       movieDetails: ''
     }
 
-    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
 
   }
 
-  handleClick () {
-    const searchTerm = document.getElementById("searchTerm").value
-    console.log("Searching for: " + searchTerm)
-    axios.get('http://www.omdbapi.com/?t=' + searchTerm + '&apikey=cac7c78e')
+  handleChange (e) {
+    e.preventDefault();
+    axios.get('http://www.omdbapi.com/?t=' + this.input.value + '&apikey=cac7c78e')
     .then(response => this.setState({movieDetails: response.data}))
   }
 
@@ -25,16 +24,16 @@ class App extends Component {
     return (
       <div className="App">
           <h1>Search by movie title</h1><br />
-          <input type="text" id="searchTerm"></input>
-          <button onClick={this.handleClick}>Search</button>
-          
+          <form onChange={this.handleChange}>
+            <input type="text" ref={(input) => this.input = input}></input>
+          </form>
           <div class="results">
             <h1>Film details</h1>
               <h3>Title: {this.state.movieDetails.Title}</h3>
               <h3>Year: {this.state.movieDetails.Year}</h3>
               <h3>Rating: {this.state.movieDetails.Rated}</h3>
               <h3>{this.state.movieDetails.Plot}</h3>
-              <img src={this.state.movieDetails.Poster} />
+              <img src={this.state.movieDetails.Poster} alt="Movie poster"/>
           </div>
       </div>
     );
